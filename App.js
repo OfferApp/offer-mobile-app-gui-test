cf27cae7de9ce1ab8fc1efbf706b435355d5635c
tejas77 <happytejasrathod@gmail.com>
2017-10-10 03:27:38 +0530

    Application Icon Change
    Changed Icons of Loading and App Icon for Better Asthetics

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import Expo from 'expo';
 import { View } from 'react-native';
 import Offers from './src/Offers';
 
  export default class App extends React.Component {
+   constructor() {
+     super();
+     this.state = {
+       isReady: false
+     };
+   }
+
+   async componentWillMount() {
+     await Expo.Font.loadAsync({
+       Roboto: require('native-base/Fonts/Roboto.ttf'),
+       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+       Ionicons: require('@expo/vector-icons/fonts/Ionicons.ttf')
+     });
+
+     this.setState({ isReady: true });
+ }
    render() {
     return (
       <View style={{ flex: 1 }}>
